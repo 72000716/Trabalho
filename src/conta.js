7e5c727641a2af9924a81bd0b051361be83ce7ce
Ninna <71091116@aluno.faculdadecotemig.br>
2021-09-07 14:56:16 -0300

    atualizando Menu01

--- src/conta.js
+++ src/conta.js
@@ -2,15 +2,15 @@ let numeroConta = 0
 let numeroOperacao = 0
 let contas = {}
 
-function gerarNumeroConta(){
+function gerarNumeroConta() {
     let numeroAtual = numeroConta.toString()
-    while(numeroAtual.length < 4){
+    while (numeroAtual.length < 4) {
         numeroAtual = '0' + numeroAtual
     }
     return numeroAtual
 }
 
-function cadastrarConta(nome, dataNascimento){
+function cadastrarConta(nome, dataNascimento) {
     let numeroDaConta = gerarNumeroConta()
     let conta = {
         nome: nome,
@@ -22,97 +22,93 @@ function cadastrarConta(nome, dataNascimento){
     return numeroDaConta
 }
 
-function contaExiste(conta){
+function contaExiste(conta) {
     return contas[conta]
 }
 
-function sacar(conta, valor){
-    if(contaExiste(conta)){
-        if(valor > 0){
-            if(contas[conta].saldo >= valor){
+function sacar(conta, valor) {
+    if (contaExiste(conta)) {
+        if (valor > 0) {
+            if (contas[conta].saldo >= valor) {
                 contas[conta].saldo -= valor
-            }else{
+            } else {
                 console.log('Saldo insuficiente')
             }
-        }else{
+        } else {
             console.log('Valor inválido')
         }
-    }else{
+    } else {
         console.log('Conta não encontrada')
     }
 }
 
-function depositar(conta, valor){
-    if(contaExiste(conta)){
-        if(valor > 0){
+function depositar(conta, valor) {
+    if (contaExiste(conta)) {
+        if (valor > 0) {
             contas[conta].saldo += valor
-        }else{
+        } else {
             console.log('Valor inválido')
         }
-    }else{
+    } else {
         console.log('Conta não encontrada')
     }
 }
 
-function informacoesDaConta(numero){
-    if(contaExiste(numero)){
+function informacoesDaConta(numero) {
+    if (contaExiste(numero)) {
         console.log(contas[numero])
-    }else{
+    } else {
         console.log('Conta não encontrada')
     }
 }
 
-function MenuPrincipal()
-        {
-            let tecla
-            cadastrarConta()
+function MenuPrincipal() {
+    let tecla
+    cadastrarConta()
 
-            do
-            {
-                Console.log()
-                Console.log("Pressione qualquer tecla para continuar")
+    do {
+        Console.log()
+        Console.log("Pressione qualquer tecla para continuar")
 
-                Console.ReadKey()
+        Console.ReadKey()
 
-                Console.Clear()
+        Console.Clear()
 
-                Console.log("----------------------")
-                Console.log("    F2    |   EXTRATO ")
-                Console.log("----------------------")
-                Console.log("    F3    |    SAQUE  ")
-                Console.log("----------------------")
-                Console.log("    F4    |  DÉPOSITO ")
-                Console.log("----------------------")
-                Console.log("   ESC    |    SAIR   ")
-                Console.log("----------------------")
-                tecla = Console.ReadKey();
-                Console.log("");
+        Console.log("----------------------")
+        Console.log("    F2    |   EXTRATO ")
+        Console.log("----------------------")
+        Console.log("    F3    |    SAQUE  ")
+        Console.log("----------------------")
+        Console.log("    F4    |  DÉPOSITO ")
+        Console.log("----------------------")
+        Console.log("   ESC    |    SAIR   ")
+        Console.log("----------------------")
+        tecla = Console.ReadKey();
+        Console.log("");
 
-                switch (tecla.Key)
-                {
-                    case ConsoleKey.F2:
-                        informacoesDaConta()                    
-                        break;
+        switch (tecla.Key) {
+            case ConsoleKey.F2:
+                informacoesDaConta()
+                break;
 
-                    case ConsoleKey.F3:
-                        sacar()
-                        break;
+            case ConsoleKey.F3:
+                sacar()
+                break;
 
-                    case ConsoleKey.F4:
-                        depositar()
-                        break;
+            case ConsoleKey.F4:
+                depositar()
+                break;
 
-                    case ConsoleKey.Escape:
-                        Console.log("Saindo do programa!")
-                        break;
+            case ConsoleKey.Escape:
+                Console.log("Saindo do programa!")
+                break;
 
-                    default:
-                        break;
-                }
-
-            } while (tecla.Key != ConsoleKey.Escape)
+            default:
+                break;
         }
 
-console.log(MenuPrincipal)
+    } while (tecla.Key != ConsoleKey.Escape)
+}
 
-    
+MenuPrincipal()
+
